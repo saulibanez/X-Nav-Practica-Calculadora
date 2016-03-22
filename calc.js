1cc93a79737009776c27424471505fa7db93bb88
saulibanez <saul.ibanez.cerro@gmail.com>
2016-03-22 17:22:40 +0100

    Calculadora V3

--- calc.js
+++ calc.js
@@ -1,5 +1,19 @@
+function trigonometria(tri, op){
+
+	if(tri=="cos"){
+		return Math.cos(op);
+	}else if(tri=="sin"){
+		return Math.sin(op);
+	}else if(tri=="tan"){
+		return Math.tan(op);
+	}
+	return;
+}
+
 $(document).ready(function() {
 	var total = "";
+	var aux = "";
+	var trigonometry=false;
 
 	$(".button").click(function(){
 		if ($(this).text() != "RESET" && $(this).text() != "=")	{
@@ -8,12 +22,28 @@ $(document).ready(function() {
 			if(valor=="zero"){
 				valor="0";
 			}
-			total += valor;
+
+			if((valor=="cos")||(valor=="sin")||(valor=="tan")){
+				$("#textWindows").append("(");
+				aux=valor;
+				trigonometry=true;
+			}else{
+				total += valor;
+			}
 		}
 
 		if ($(this).text() == "=") {
-			$("#textWindows").text(eval(total));
-			total=eval(total);
+			if(trigonometry){
+				total=trigonometria(aux, total);
+				$("#textWindows").text(total);
+				trigonometry=false;
+				console.log(total);
+
+			}else{
+				$("#textWindows").text(eval(total));
+				total=eval(total);
+				console.log(total);
+			}
 		}
 
 		if($(this).text() == "RESET") {
@@ -22,24 +52,41 @@ $(document).ready(function() {
 		}
 	});
 
+	var tri="";
 	$(document).keypress(function(e){
 		var tecla=e.which;
-
+		
 		if(tecla==13){
-			$("#textWindows").text(eval(total));
-			total=eval(total);
+			if(trigonometry){
+				total=trigonometria(aux, total);
+				$("#textWindows").text(total);
+				trigonometry=false;
+			}else{
+				$("#textWindows").text(eval(total));
+				total=eval(total);
+			}
 		}
 
 		if(tecla==114){
 			$("#textWindows").text("");
 			total="";
+			tri="";
 		}
 
 		if((tecla > 47)&&(tecla<58)||(tecla==42)||(tecla==43)||(tecla==45)||(tecla==47)){
 			var valor=String.fromCharCode(tecla);
 			$("#textWindows").append(valor);
 			total += valor;
+		}else if((tecla==115)||(tecla==105)||(tecla==110)||
+			(tecla==99)||(tecla==111)||(tecla==116)||(tecla==97)||(tecla==110)){
+				tri += String.fromCharCode(tecla);
 		}
-		console.log(tecla);
+
+		if((tri == "sin")||(tri == "cos")||(tri == "tan")){
+			$("#textWindows").append(tri+"(");
+			aux=tri;
+			trigonometry=true;
+			tri="";
+		}
 	});
 });
